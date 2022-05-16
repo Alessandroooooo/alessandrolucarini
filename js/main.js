ebdfc5cd9182f9f31cab4f7fb2f3c196c894d5c6
Leonardo Almeida <leo@leleo.com.br>
2022-05-16 19:36:24 +0200

    js fix

--- js/main.js
+++ js/main.js
@@ -9,11 +9,8 @@ carousels.forEach((carousel) => {
   });
 });
 
-document.getElementById("publications").style.display = "none";
-document.getElementById("videos").style.display = "none";
-document.getElementById("posters").style.display = "none";
-
 const clickity = (prop) => {
+  console.log("foi");
   document.getElementById("publications").style.display = "none";
   document.getElementById("videos").style.display = "none";
   document.getElementById("posters").style.display = "none";
@@ -42,3 +39,70 @@ const showModal = (prop, elem) => {
     elem.style.color = "#00a00c";
   }
 };
+
+document.getElementById("pub-button").addEventListener(
+  "click",
+  function () {
+    document.getElementById("videos").style.display = "none";
+    document.getElementById("posters").style.display = "none";
+    document.getElementById("publications").style.display = "block";
+
+    const carousels = document.querySelectorAll(".main-carousel");
+
+    carousels.forEach((carousel) => {
+      new Flickity(carousel, {
+        cellAlign: "left",
+        contain: true,
+        freeScroll: true,
+        pageDots: false,
+      });
+    });
+  },
+  false
+);
+
+document.getElementById("vid-button").addEventListener(
+  "click",
+  function () {
+    document.getElementById("publications").style.display = "none";
+    document.getElementById("posters").style.display = "none";
+    document.getElementById("videos").style.display = "block";
+
+    const carousels = document.querySelectorAll(".main-carousel");
+
+    carousels.forEach((carousel) => {
+      new Flickity(carousel, {
+        cellAlign: "left",
+        contain: true,
+        freeScroll: true,
+        pageDots: false,
+      });
+    });
+  },
+  false
+);
+
+document.getElementById("pos-button").addEventListener(
+  "click",
+  function () {
+    document.getElementById("videos").style.display = "none";
+    document.getElementById("publications").style.display = "none";
+    document.getElementById("posters").style.display = "block";
+
+    const carousels = document.querySelectorAll(".main-carousel");
+
+    carousels.forEach((carousel) => {
+      new Flickity(carousel, {
+        cellAlign: "left",
+        contain: true,
+        freeScroll: true,
+        pageDots: false,
+      });
+    });
+  },
+  false
+);
+
+document.getElementById("publications").style.display = "none";
+document.getElementById("videos").style.display = "none";
+document.getElementById("posters").style.display = "none";
